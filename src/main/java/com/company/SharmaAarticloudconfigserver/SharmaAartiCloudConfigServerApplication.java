6627425e156398bc7f7969993ad2d05c00592d68
Aarti <aarti@dhcp-10-101-219-152.wireless.northwestern.private>
2019-08-15 14:23:35 -0500

    new proj

---
+++ src/main/java/com/company/SharmaAarticloudconfigserver/SharmaAartiCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.company.SharmaAarticloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharmaAartiCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharmaAartiCloudConfigServerApplication.class, args);
+	}
+
+}
